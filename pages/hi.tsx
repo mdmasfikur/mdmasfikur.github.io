0b472324fdea6a99dab2c15c9e1ef67fea1d378d
MdMasfikurRahman <masfikur15-4239@diu.edu.bd>
2022-12-17 00:42:49 +0600

    hi

--- pages/hi.tsx
+++ pages/hi.tsx
@@ -1,9 +1,8 @@
 
 export default function Hi() {
-    return (
-      <div className="bg-gray-500">
-        <h3 className="font">Hi World</h3>
-      </div>
-    )
-  }
-  
+  return (
+    <div className="bg-gray-500">
+      <h3 className="font">Hi World</h3>
+    </div>
+  )
+}
