fe22b5c402ff0e1e8b988c6b97ab0c0d06227829
MdMasfikurRahman <masfikur15-4239@diu.edu.bd>
2022-12-18 16:52:32 +0600

    removed test

--- pages/feature/[feature].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Features() {
-    const router = useRouter();
-    const feature = router.query.feature;
-    return (
-        <h3>Feature: {feature}</h3>
-    );
-}
--- pages/feature/index.tsx
+++
@@ -1,7 +0,0 @@
-
-export default function Features(){
-return (
-    <h3>Main Feature Page</h3>
-
-);
-}
